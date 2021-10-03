04c0764f63f777f35e7f507218bf7b9d5f70217e
morteza yaghoobi <morteza.yaghoobi1375@gmail.com>
2021-10-03 16:37:44 +0330

    build a clean architecture structure

--- app/src/main/java/com/example/justasimpletodo/feature_Todo/presentation/MainActivity.kt
+++ app/src/main/java/com/example/justasimpletodo/feature_Todo/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.justasimpletodo
+package com.example.justasimpletodo.feature_Todo.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.justasimpletodo.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
