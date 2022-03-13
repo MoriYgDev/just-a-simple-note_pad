5aa8258d1352377a968594f7ab7c40b79cb9bfaa
morteza yaghoobi <morteza.yaghoobi1375@gmail.com>
2022-03-13 18:42:44 +0330

    implement DeleteNotes UseCase class

---
+++ app/src/main/java/ir/moris/justasimpletodoapp/feature_note/domain/use_case/DeleteNote.kt
@@ -0,0 +1,12 @@
+package ir.moris.justasimpletodoapp.feature_note.domain.use_case
+
+import ir.moris.justasimpletodoapp.feature_note.domain.model.Note
+import ir.moris.justasimpletodoapp.feature_note.domain.repository.NoteRepository
+
+class DeleteNote(private val repository: NoteRepository) {
+
+    suspend operator fun invoke(note: Note){
+        repository.deleteNote(note)
+    }
+
+}
